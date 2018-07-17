a2d740cfe3c53af4b51e7b44ebf0e13a47b38e24
divyanshyadav11 <40422869+divyanshyadav11@users.noreply.github.com>
2018-07-17 13:28:18 +0530

    Update production.py
    ghrth

--- shop_system/shop_system/settings/production.py
+++ shop_system/shop_system/settings/production.py
@@ -16,8 +16,8 @@ DATABASES = {
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'rohanyadav84@gmail.com'
-EMAIL_HOST_PASSWORD = '123456dyadav'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 
 STATIC_URL = '/static/'
